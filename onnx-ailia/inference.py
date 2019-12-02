91e14f4ad27bbb618e3e41fea9610dda9de4ae21
客野 一樹 <kyakuno@axell.co.jp>
2019-12-02 10:17:31 +0900

    Remove unused category

--- onnx-ailia/inference.py
+++ onnx-ailia/inference.py
@@ -57,24 +57,6 @@ for i in range(cnt):
 	end=int(round(time.time() * 1000))
 	print("## ailia processing time , "+str(i)+" , "+str(end-start)+" ms")
 
-# category
-coco_category=[
-	"person", "bicycle", "car", "motorcycle", "airplane", "bus", "train",
-	"truck", "boat", "traffic light", "fire hydrant", "stop sign",
-	"parking meter", "bench", "bird", "cat", "dog", "horse", "sheep", "cow",
-	"elephant", "bear", "zebra", "giraffe", "backpack", "umbrella",
-	"handbag", "tie", "suitcase", "frisbee", "skis", "snowboard",
-	"sports ball", "kite", "baseball bat", "baseball glove", "skateboard",
-	"surfboard", "tennis racket", "bottle", "wine glass", "cup", "fork",
-	"knife", "spoon", "bowl", "banana", "apple", "sandwich", "orange",
-	"broccoli", "carrot", "hot dog", "pizza", "donut", "cake", "chair",
-	"couch", "potted plant", "bed", "dining table", "toilet", "tv",
-	"laptop", "mouse", "remote", "keyboard", "cell phone", "microwave",
-	"oven", "toaster", "sink", "refrigerator", "book", "clock", "vase",
-	"scissors", "teddy bear", "hair drier", "toothbrush"
-]
-#class_names=coco_category
-
 # get result
 count = detector.get_object_count()
 
